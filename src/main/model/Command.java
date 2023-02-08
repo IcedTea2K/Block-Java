b9bbad3702e0d6c47d964bb09403a60e4b9dacf6
Minh Au <auduongminh2004@gmail.com>
2023-02-07 22:00:34 -0800

    create an interface for Command

---
+++ src/main/model/Command.java
@@ -0,0 +1,19 @@
+package model;
+
+// Interface for Block Java Commands
+public interface Command {
+    // MODIFIES: this
+    // EFFECTS: if the data type fits the constraints specified for
+    //          this particular command, store inputs for the command and return true
+    //          Otherwise, return false and do nothing
+    public boolean input(DataType... inputs);
+
+    // REQUIRES: valid input have already been stored (with input() that returns true)
+    // MODIFIES: this
+    // EFFECTS: execute the command with the current
+    public void execute();
+
+    // REQUIRES: only after the command has executed at least once
+    // EFFECTS: return the result of the command
+    public DataType getResult();
+}
