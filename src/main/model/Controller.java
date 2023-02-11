b6178f814d7d5bc0dee7a3de2b22d86ae4fbaab7
Minh Au <auduongminh2004@gmail.com>
2023-02-11 15:09:55 -0800

    add abstract method getStream

--- src/main/model/Controller.java
+++ src/main/model/Controller.java
@@ -1,2 +1,9 @@
-package model;public class Controller {
+package model;
+
+import java.util.List;
+
+// Type of commands that control the flow of the user's program
+public abstract class Controller implements Command {
+    // EFFECT: return command stream that this controller contains
+    public abstract List<Command> getStream();
 }
