55370788e954dc0fd855bfad0a4727dbb4c9bba2
Minh Au <auduongminh2004@gmail.com>
2023-02-11 15:11:03 -0800

    DataType can now represent the stream of DataType

--- src/main/model/DataType.java
+++ src/main/model/DataType.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // The data that commands will be working with.
@@ -7,20 +8,35 @@ import java.util.List;
 public class DataType {
     private int number;
     private boolean bool;
+    private List<DataType> stream;
 
+    // EFFECTS: store a stream of data
+    public DataType(List<DataType> stream) {
+        this.stream = new ArrayList<>(stream);
+    }
+
+    // EFFECTS: store a number as data
     public DataType(int number) {
         this.number = number;
     }
 
+    // EFFECTS: store a boolean as data
     public DataType(boolean bool) {
         this.bool = bool;
     }
 
+    // EFFECTS: return data if it's represented as number
     public int getNumber() {
         return this.number;
     }
 
+    // EFFECTS: return data if it's represented as boolean
     public boolean getBoolean() {
         return this.bool;
+    }
+
+    // EFFECTS: return data if it's a represented as a stream of data
+    public List<DataType> getResultStream() {
+        return new ArrayList<>(this.stream);
     }
 }
