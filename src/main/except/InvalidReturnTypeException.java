8af6390c51037686fccf238c18cfcdf65539fb1f
Minh Au <auduongminh2004@gmail.com>
2023-02-12 12:10:11 -0800

    add string-type to DataType to specify what the DataType is when the InvalidReturnTypeException is raised

--- src/main/except/InvalidReturnTypeException.java
+++ src/main/except/InvalidReturnTypeException.java
@@ -2,5 +2,8 @@ package except;
 
 // Exception for when a DataType tries to access the wrong data
 // that it represents
-public class InvalidReturnTypeException extends Exception{
+public class InvalidReturnTypeException extends Exception {
+    public InvalidReturnTypeException(String expectedType) {
+        super(expectedType);
+    }
 }
--- src/main/model/DataType.java
+++ src/main/model/DataType.java
@@ -11,26 +11,30 @@ public class DataType {
     private Integer number;
     private Boolean bool;
     private List<DataType> dataStream;
+    private String type;
 
     // EFFECTS: store a stream of data
     public DataType(List<DataType> dataStream) {
         this.dataStream = new ArrayList<>(dataStream);
+        this.type = "data stream";
     }
 
     // EFFECTS: store a number as data
     public DataType(int number) {
         this.number = number;
+        this.type = "number";
     }
 
     // EFFECTS: store a boolean as data
     public DataType(boolean bool) {
         this.bool = bool;
+        this.type = "boolean";
     }
 
     // EFFECTS: return data if it's represented as number
     public int getNumber() throws InvalidReturnTypeException {
         if (this.number == null) {
-            throw new InvalidReturnTypeException();
+            throw new InvalidReturnTypeException(this.type);
         }
         return this.number;
     }
@@ -38,7 +42,7 @@ public class DataType {
     // EFFECTS: return data if it's represented as boolean
     public boolean getBoolean() throws InvalidReturnTypeException {
         if (this.bool == null) {
-            throw new InvalidReturnTypeException();
+            throw new InvalidReturnTypeException(this.type);
         }
         return this.bool;
     }
@@ -46,7 +50,7 @@ public class DataType {
     // EFFECTS: return data if it's a represented as a stream of data
     public List<DataType> getDataStream() throws InvalidReturnTypeException {
         if (this.dataStream == null) {
-            throw new InvalidReturnTypeException();
+            throw new InvalidReturnTypeException(this.type);
         }
         return new ArrayList<>(this.dataStream); // clone the data
     }
