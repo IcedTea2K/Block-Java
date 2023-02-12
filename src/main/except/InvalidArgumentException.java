c79355d41b6c90b9dd4961901c4c914b3b426292
Minh Au <auduongminh2004@gmail.com>
2023-02-12 12:10:45 -0800

    modify exception message

--- src/main/except/InvalidArgumentException.java
+++ src/main/except/InvalidArgumentException.java
@@ -11,6 +11,6 @@ public class InvalidArgumentException extends Exception {
     // EFFECTS: construct exception due to the invalid argument type
     public InvalidArgumentException(String givenType, String expectedType) {
         super("Invalid argument type:\n"
-                + "Expecting " + expectedType + "received " + givenType);
+                + "Expecting " + expectedType + "Received " + givenType);
     }
 }
