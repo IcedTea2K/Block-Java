08cb5e1a898cf2b92bdb872a37322c0b729b8ef5
Minh Au <auduongminh2004@gmail.com>
2023-02-11 16:56:55 -0800

    add operator abstract class and a simple implementation of getConstraints

---
+++ src/main/model/Operator.java
@@ -0,0 +1,10 @@
+package model;
+
+public abstract class Operator implements Command {
+
+    @Override
+    // EFFECTS: return the constraints specific to this command
+    public String getConstraints() {
+        return "Operators only accept two inputs";
+    }
+}
