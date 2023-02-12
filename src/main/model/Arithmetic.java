04fd01000ae5976fccb0daa7d8f0338530e1d9a2
Minh Au <auduongminh2004@gmail.com>
2023-02-12 09:38:35 -0800

    add abstract method getInputs

--- src/main/model/Arithmetic.java
+++ src/main/model/Arithmetic.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.List;
+
 // Type of commands that perform on numbers and return a number
 public abstract class Arithmetic extends Operator {
     @Override
@@ -7,4 +9,7 @@ public abstract class Arithmetic extends Operator {
     public String getReturnType() {
         return "Number";
     }
+
+    // EFFECTS: return the two inputs that were given to the command
+    public abstract List<DataType> getInputs();
 }
