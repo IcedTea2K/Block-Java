9cbaae12f51e5376cf9c4af3bd75da80fefecd29
Minh Au <auduongminh2004@gmail.com>
2023-02-12 11:52:19 -0800

    refractor the exception in case of invalid number of inputs to account for both too few and too many inputs

--- src/main/except/InvalidArgumentException.java
+++ src/main/except/InvalidArgumentException.java
@@ -2,16 +2,10 @@ package except;
 
 // Exception for when user's input to a command is invalid
 public class InvalidArgumentException extends Exception {
-    // EFFECTS: construct exception due to lack of arguments
+    // EFFECTS: construct exception due to invalid number of inputs
     public InvalidArgumentException(int numberOfInputs, int expectedInputs) {
-        super("Too few arguments:\n"
-                + "Expecting " + expectedInputs + " Received " + numberOfInputs);
-    }
-
-    // EFFECTS: construct exception due to the exceeding number of arguments
-    public InvalidArgumentException(int numberOfInputs) {
-        super("Exceeds the number of arguments:\n"
-                + "Expecting 2 Received " + numberOfInputs);
+        super(numberOfInputs < expectedInputs ? "Too few arguments:\n" : "Exceeds the number of arguments:\n"
+            + "Expecting " + expectedInputs + " Received " + numberOfInputs);
     }
 
     // EFFECTS: construct exception due to the invalid argument type
