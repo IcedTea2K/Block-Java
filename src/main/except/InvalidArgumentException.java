9c86251bb2ef020fe4db739f88efe258e5927818
Minh Au <auduongminh2004@gmail.com>
2023-02-12 09:21:36 -0800

    add constructor for too few argument case; modify the string of the others

--- src/main/except/InvalidArgumentException.java
+++ src/main/except/InvalidArgumentException.java
@@ -2,15 +2,21 @@ package except;
 
 // Exception for when user's input to a command is invalid
 public class InvalidArgumentException extends Exception {
+    // EFFECTS: construct exception due to lack of arguments
+    public InvalidArgumentException(int numberOfInputs, int expectedInputs) {
+        super("Too few arguments:\n"
+                + "Expecting " + expectedInputs + " Received " + numberOfInputs);
+    }
+
     // EFFECTS: construct exception due to the exceeding number of arguments
     public InvalidArgumentException(int numberOfInputs) {
-        super("Exceeds the number of arguments:\n "
-                + "Expecting 2 Received " + Integer.toString(numberOfInputs));
+        super("Exceeds the number of arguments:\n"
+                + "Expecting 2 Received " + numberOfInputs);
     }
 
     // EFFECTS: construct exception due to the invalid argument type
     public InvalidArgumentException(String givenType, String expectedType) {
-        super("Invalid argument type: \n "
+        super("Invalid argument type:\n"
                 + "Expecting " + expectedType + "received " + givenType);
     }
 }
