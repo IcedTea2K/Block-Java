98b9b30ef746f3c38170ddc7f72bf3ab067dba91
Minh Au <auduongminh2004@gmail.com>
2023-02-11 16:33:33 -0800

    convert num and bool to Integer and Boolean to compare with null; implemenet the DataType class

--- src/main/model/DataType.java
+++ src/main/model/DataType.java
@@ -8,8 +8,8 @@ import java.util.List;
 // The data that commands will be working with.
 // It acts as a wrapper class for int, boolean, and their list<>
 public class DataType {
-    private int number;
-    private boolean bool;
+    private Integer number;
+    private Boolean bool;
     private List<DataType> dataStream;
 
     // EFFECTS: store a stream of data
@@ -29,16 +29,25 @@ public class DataType {
 
     // EFFECTS: return data if it's represented as number
     public int getNumber() throws InvalidReturnTypeException {
+        if (this.number == null) {
+            throw new InvalidReturnTypeException();
+        }
         return this.number;
     }
 
     // EFFECTS: return data if it's represented as boolean
     public boolean getBoolean() throws InvalidReturnTypeException {
+        if (this.bool == null) {
+            throw new InvalidReturnTypeException();
+        }
         return this.bool;
     }
 
     // EFFECTS: return data if it's a represented as a stream of data
     public List<DataType> getDataStream() throws InvalidReturnTypeException {
-        return new ArrayList<>(this.dataStream);
+        if (this.dataStream == null) {
+            throw new InvalidReturnTypeException();
+        }
+        return new ArrayList<>(this.dataStream); // clone the data
     }
 }
