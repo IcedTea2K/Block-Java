1145c87d1d86abdaa5c5dc81e733c7752a6b7ca9
Minh Au <auduongminh2004@gmail.com>
2023-02-11 17:13:41 -0800

    add specification getReturnType

--- src/main/model/Arithmetic.java
+++ src/main/model/Arithmetic.java
@@ -2,6 +2,7 @@ package model;
 
 public abstract class Arithmetic extends Operator {
     @Override
+    // EFFECTS: return the return type of the command
     public String getReturnType() {
         return "Number";
     }
