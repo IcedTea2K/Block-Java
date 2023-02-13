bd7cfc21be648e1efa0d5cb0cc5a1e5aa7a31921
Minh Au <auduongminh2004@gmail.com>
2023-02-13 08:25:30 -0800

    move methods to abstract class for more abstraction

--- src/main/model/Add.java
+++ src/main/model/Add.java
@@ -10,21 +10,6 @@ import java.util.List;
 
 // Add two numbers together
 public class Add extends Arithmetic {
-    private DataType numOne;
-    private DataType numTwo;
-    private DataType result;
-
-    @Override
-    // MODIFIES: this
-    // EFFECTS: take in inputs
-    //          If the number of inputs is more than 2 or the type of input is not number,
-    //          InvalidArgumentException will be raised.
-    public void input(DataType... inputs) throws InvalidArgumentException {
-        checkInput(inputs);
-        numOne = inputs[0];
-        numTwo = inputs[1];
-    }
-
     @Override
     // MODIFIES: this
     // EFFECTS: add the two given numbers
@@ -35,16 +20,6 @@ public class Add extends Arithmetic {
         int tempNumOne = retrieveNumber(numOne);
         int tempNumTwo = retrieveNumber(numTwo);
         result = new DataType(tempNumOne + tempNumTwo);
-    }
-
-    @Override
-    // EFFECTS: return the result of the addition
-    //          If the command has yet to executed, throw NotYetExecutedException
-    public DataType getResult() throws NotYetExecutedException {
-        if (result == null) {
-            throw new NotYetExecutedException();
-        }
-        return result;
     }
 
     @Override
@@ -66,49 +41,5 @@ public class Add extends Arithmetic {
 
         String msg = "int result = " + tempNumOne + " + " + tempNumTwo + ";";
         return msg;
-    }
-
-    @Override
-    // EFFECTS: return the inputs of this command
-    public List<DataType> getInputs() throws MissingArgumentException {
-        checkCurrentInputs();
-
-        List<DataType> tempInputs = new ArrayList<>();
-        tempInputs.add(numOne);
-        tempInputs.add(numTwo);
-        return tempInputs;
-    }
-
-    // EFFECTS: if there's only two inputs, and they are both numbers, do nothing.
-    //          Otherwise, throw InvalidArgumentException
-    private void checkInput(DataType[] inputs) throws InvalidArgumentException {
-        if (inputs.length != 2) {
-            throw new InvalidArgumentException(inputs.length, 2);
-        }
-
-        try {
-            int tempNumOne = inputs[0].getNumber();
-            int tempNumTwo = inputs[1].getNumber();
-        } catch (InvalidReturnTypeException e) {
-            throw new InvalidArgumentException(e.getMessage(), "number");
-        }
-    }
-
-    // EFFECTS: return the number the DataType is representing
-    private int retrieveNumber(DataType num) {
-        int tempNum = 0;
-        try {
-            tempNum = num.getNumber();
-        } catch (InvalidReturnTypeException ignore) {
-            // do nothing
-        }
-        return tempNum;
-    }
-
-    // EFFECTS: verify the current inputs
-    private void checkCurrentInputs() throws MissingArgumentException {
-        if (numOne == null || numTwo == null) {
-            throw new MissingArgumentException();
-        }
     }
 }
--- src/main/model/Arithmetic.java
+++ src/main/model/Arithmetic.java
@@ -1,12 +1,19 @@
 package model;
 
 import except.InvalidArgumentException;
+import except.InvalidReturnTypeException;
 import except.MissingArgumentException;
+import except.NotYetExecutedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // Type of commands that perform on numbers and return a number
 public abstract class Arithmetic extends Operator {
+    protected DataType numOne;
+    protected DataType numTwo;
+    protected DataType result;
+
     @Override
     // EFFECTS: return the return type of the command
     public String getReturnType() {
@@ -19,6 +26,67 @@ public abstract class Arithmetic extends Operator {
         return super.getConstraints() +  " They both need to be numbers.";
     }
 
-    // EFFECTS: return the two inputs that were given to the command
-    public abstract List<DataType> getInputs() throws MissingArgumentException;
+    @Override
+    // MODIFIES: this
+    // EFFECTS: take in inputs
+    //          If the number of inputs is more than 2 or the type of input is not number,
+    //          InvalidArgumentException will be raised.
+    public void input(DataType... inputs) throws InvalidArgumentException {
+        checkInput(inputs);
+        numOne = inputs[0];
+        numTwo = inputs[1];
+    }
+
+    @Override
+    // EFFECTS: return the result of the addition
+    //          If the command has yet to executed, throw NotYetExecutedException
+    public DataType getResult() throws NotYetExecutedException {
+        if (result == null) {
+            throw new NotYetExecutedException();
+        }
+        return result;
+    }
+
+    // EFFECTS: return the inputs of this command
+    public List<DataType> getInputs() throws MissingArgumentException {
+        checkCurrentInputs();
+
+        List<DataType> tempInputs = new ArrayList<>();
+        tempInputs.add(numOne);
+        tempInputs.add(numTwo);
+        return tempInputs;
+    }
+
+    // EFFECTS: if there's only two inputs, and they are both numbers, do nothing.
+    //          Otherwise, throw InvalidArgumentException
+    protected void checkInput(DataType[] inputs) throws InvalidArgumentException {
+        if (inputs.length != 2) {
+            throw new InvalidArgumentException(inputs.length, 2);
+        }
+
+        try {
+            int tempNumOne = inputs[0].getNumber();
+            int tempNumTwo = inputs[1].getNumber();
+        } catch (InvalidReturnTypeException e) {
+            throw new InvalidArgumentException(e.getMessage(), "number");
+        }
+    }
+
+    // EFFECTS: return the number the DataType is representing
+    protected int retrieveNumber(DataType num) {
+        int tempNum = 0;
+        try {
+            tempNum = num.getNumber();
+        } catch (InvalidReturnTypeException ignore) {
+            // do nothing
+        }
+        return tempNum;
+    }
+
+    // EFFECTS: verify the current inputs
+    protected void checkCurrentInputs() throws MissingArgumentException {
+        if (numOne == null || numTwo == null) {
+            throw new MissingArgumentException();
+        }
+    }
 }
