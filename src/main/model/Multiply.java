a37f41e744341c4a5d1fffc3f6f98f845fa45b61
Minh Au <auduongminh2004@gmail.com>
2023-02-13 22:41:45 -0800

    implement Multiply

--- src/main/model/Multiply.java
+++ src/main/model/Multiply.java
@@ -2,19 +2,38 @@ package model;
 
 import except.MissingArgumentException;
 
+// A command to multiply two numbers
 public class Multiply extends Arithmetic {
     @Override
+    // MODIFIES: this
+    // EFFECTS: multiply the two numbers
+    //          If either input is missing, throw MissingArgumentException
     public void execute() throws MissingArgumentException {
-        
+        checkCurrentInputs();
+
+        int tempNumOne = retrieveNumber(numOne);
+        int tempNumTwo = retrieveNumber(numTwo);
+        result = new DataType(tempNumOne * tempNumTwo);
     }
 
     @Override
+    // EFFECTS: return some examples of this command
     public String getExamples() {
-        return null;
+        String msg = "MUL 12 29\n"
+                + "MUL 83.1 -12\n"
+                + "MUL -31 -78";
+        return msg;
     }
 
     @Override
+    // EFFECTS: return the Java code representation of this command
+    //          with the given inputs.
     public String getJava() throws MissingArgumentException {
-        return null;
+        checkCurrentInputs();
+        int tempNumOne = retrieveNumber(numOne);
+        int tempNumTwo = retrieveNumber(numTwo);
+
+        String msg = "int result = " + tempNumOne + " * " + tempNumTwo + ";";
+        return msg;
     }
 }
