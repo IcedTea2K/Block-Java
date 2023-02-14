e0a46efcfe0a71bb8c1441216aced4ff7bcee22b
Minh Au <auduongminh2004@gmail.com>
2023-02-13 22:03:47 -0800

    add skeleton class for subtract

---
+++ src/main/model/Subtract.java
@@ -0,0 +1,20 @@
+package model;
+
+import except.MissingArgumentException;
+
+public class Subtract extends Arithmetic {
+    @Override
+    public void execute() throws MissingArgumentException {
+        
+    }
+
+    @Override
+    public String getExamples() {
+        return null;
+    }
+
+    @Override
+    public String getJava() throws MissingArgumentException {
+        return null;
+    }
+}
