4ff26c54974785b76bc044000e92707640d15143
Minh Au <auduongminh2004@gmail.com>
2023-02-13 22:23:25 -0800

    implement subtract command

--- src/main/model/Subtract.java
+++ src/main/model/Subtract.java
@@ -4,17 +4,35 @@ import except.MissingArgumentException;
 
 public class Subtract extends Arithmetic {
     @Override
+    // MODIFIES: this
+    // EFFECTS: subtract the second number from the first number
+    //          If either input is missing, throw MissingArgumentException
     public void execute() throws MissingArgumentException {
-        
+        checkCurrentInputs();
+
+        int tempNumOne = retrieveNumber(numOne);
+        int tempNumTwo = retrieveNumber(numTwo);
+        result = new DataType(tempNumOne - tempNumTwo);
     }
 
     @Override
+    // EFFECTS: return some examples of this command
     public String getExamples() {
-        return null;
+        String msg = "SUB 1337 89\n"
+                + "SUB 28.1 39\n"
+                + "SUB 38 -10";
+        return msg;
     }
 
     @Override
+    // EFFECTS: return the Java code representation of this command
+    //          with the given inputs.
     public String getJava() throws MissingArgumentException {
-        return null;
+        checkCurrentInputs();
+        int tempNumOne = retrieveNumber(numOne);
+        int tempNumTwo = retrieveNumber(numTwo);
+
+        String msg = "int result = " + tempNumOne + " - " + tempNumTwo + ";";
+        return msg;
     }
 }
