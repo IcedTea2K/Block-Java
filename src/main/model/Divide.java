cd34ae10332733fbca8779be6614d3b50d8f8bff
Minh Au <auduongminh2004@gmail.com>
2023-02-19 14:02:23 -0800

    override checkInput

--- src/main/model/Divide.java
+++ src/main/model/Divide.java
@@ -1,13 +1,11 @@
 package model;
 
-import except.DivideByZeroException;
-import except.InvalidArgumentException;
-import except.MissingArgumentException;
+import except.*;
 
 public class Divide extends Arithmetic {
     @Override
     public void input(DataType... inputs) throws InvalidArgumentException {
-//        throw new InvalidArgumentException(2, 2);
+
     }
 
     @Override
@@ -23,5 +21,24 @@ public class Divide extends Arithmetic {
     @Override
     public String getJava() throws MissingArgumentException {
         return null;
+    }
+
+    @Override
+    // EFFECTS: If either of the inputs is not a number, throw WrongArgumentTypeException
+    //          If the number of inputs don't match the constraints, throw
+    //          UnexpectedNumberOfArgumentsException.
+    //          If the second number (denominator), throw DivideByZeroException
+    //          Otherwise, do nothing
+    protected void checkInput(DataType[] inputs) throws InvalidArgumentException {
+        super.checkInput(inputs);
+
+        try {
+            int tempDenominator = inputs[1].getNumber();
+            if (tempDenominator == 0) {
+                throw new DivideByZeroException();
+            }
+        } catch (InvalidReturnTypeException e) {
+            assert true; // no exception should be raised
+        }
     }
 }
