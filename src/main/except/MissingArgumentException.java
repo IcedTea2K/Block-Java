75e6deab4d261cdbf8f9cbfa01a015e4d5ae71e5
Minh Au <auduongminh2004@gmail.com>
2023-02-20 15:41:05 -0800

    make the exception a RunTimeErrorException

--- src/main/except/MissingArgumentException.java
+++ src/main/except/MissingArgumentException.java
@@ -2,7 +2,7 @@ package except;
 
 // Exception for when the command ties to perform certain operations without
 // any input
-public class MissingArgumentException extends Exception {
+public class MissingArgumentException extends RuntimeException {
     // EFFECTS: construct the exception with the exception message
     public MissingArgumentException() {
         super("No argument has been given");
