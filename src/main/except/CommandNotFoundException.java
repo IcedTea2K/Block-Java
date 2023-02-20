38f14a26dce534166a539db6d1e835e8ddb7f811
Minh Au <auduongminh2004@gmail.com>
2023-02-20 13:52:46 -0800

    edit exception message

--- src/main/except/CommandNotFoundException.java
+++ src/main/except/CommandNotFoundException.java
@@ -2,7 +2,7 @@ package except;
 
 // Exception for when user tries to access an unavailable command
 public class CommandNotFoundException extends Exception {
-    public CommandNotFoundException(String msg) {
-        super(msg);
+    public CommandNotFoundException(int idx) {
+        super("No command is found at index #" + idx);
     }
 }
