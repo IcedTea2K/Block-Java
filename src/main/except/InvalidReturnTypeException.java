49b439782fc7174ef5202b34187d710d0bbb486f
Minh Au <auduongminh2004@gmail.com>
2023-02-20 15:43:18 -0800

    make the exception a RunTimeErrorException

--- src/main/except/InvalidReturnTypeException.java
+++ src/main/except/InvalidReturnTypeException.java
@@ -2,7 +2,7 @@ package except;
 
 // Exception for when a DataType tries to access the wrong data
 // that it represents
-public class InvalidReturnTypeException extends Exception {
+public class InvalidReturnTypeException extends RuntimeException {
     public InvalidReturnTypeException(String expectedType) {
         super(expectedType);
     }
