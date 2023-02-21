593c0b3ed99fc1edcb45c6bc947967c78ddf541c
Minh Au <auduongminh2004@gmail.com>
2023-02-20 17:15:20 -0800

    add startMenu

--- src/main/ui/ConsoleApplication.java
+++ src/main/ui/ConsoleApplication.java
@@ -13,15 +13,32 @@ public class ConsoleApplication {
     public ConsoleApplication() {
         this.mainTranslator = new Translator();
         this.scanner = new Scanner(System.in);
+
+        run();
     }
 
     // EFFECTS: run the program
     private void run() {
-
+        startMenu();
     }
 
     // EFFECTS: process the inputs and call the corresponding commands
     private void processInput() {
 
+    }
+
+    // EFFECTS: display the start menu
+    private void startMenu() {
+        String menu = ""
+                + "#########################################################\n"
+                + "#                Welcome to BlockJava                   #\n"
+                + "# This is a program that helps introduce user to        #\n"
+                + "# programming as well as Java. Type 'HELP' to see the   #\n"
+                + "# the available commands.                               #\n"
+                + "#                                                       #\n"
+                + "# **Note** BlockJava is still in early development.     #\n"
+                + "# Full version with graphical interface is coming soon. #\n"
+                + "#########################################################\n";
+        System.out.println(menu);
     }
 }
