7464924ae9a3f0cc4b6351e40df5c6f34c6d2eb9
Minh Au <auduongminh2004@gmail.com>
2023-02-20 17:03:34 -0800

    add stubs

--- src/main/ui/ConsoleApplication.java
+++ src/main/ui/ConsoleApplication.java
@@ -1,8 +1,27 @@
 package ui;
 
+import model.Translator;
+
+import java.util.Scanner;
+
 // UI for Block Java on the console
 public class ConsoleApplication {
+    private Translator mainTranslator;
+    private Scanner scanner;
+
+    // EFFECTS: construct and run the application
     public ConsoleApplication() {
+        this.mainTranslator = new Translator();
+        this.scanner = new Scanner(System.in);
+    }
+
+    // EFFECTS: run the program
+    private void run() {
+
+    }
+
+    // EFFECTS: process the inputs and call the corresponding commands
+    private void processInput() {
 
     }
 }
