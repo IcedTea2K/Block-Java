bb6458fe42fbe6051f3f5c45ced60191e1832639
Minh Au <auduongminh2004@gmail.com>
2023-02-25 15:27:31 -0800

    add class comments

--- src/main/except/DivideByZeroException.java
+++ src/main/except/DivideByZeroException.java
@@ -1,5 +1,6 @@
 package except;
 
+// Exception for when the user tries to divide by 0
 public class DivideByZeroException extends InvalidArgumentException {
     public DivideByZeroException() {
         super("The denominator cannot be zero");
