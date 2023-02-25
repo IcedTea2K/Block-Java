5ab6fb672d46b89055520e7c018c65eb4113e01b
Minh Au <auduongminh2004@gmail.com>
2023-02-25 15:24:11 -0800

    temporarily remove bool, comparison, controller, logical

--- src/main/model/Logical.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-// Type of commands that perform logical operations and return a boolean
-public abstract class Logical extends Bool {
-    @Override
-    public String getConstraints() {
-        return super.getConstraints() + "The two inputs must be of type boolean";
-    }
-}
