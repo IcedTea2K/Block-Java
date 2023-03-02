873e2c34e8199919d7438fdb9c8c058613e9fa9c
Minh Au <auduongminh2004@gmail.com>
2023-03-01 22:55:13 -0800

    add skeleton code for Saver Class

---
+++ src/main/persistence/Saver.java
@@ -0,0 +1,32 @@
+package persistence;
+
+import model.Command;
+import java.util.List;
+import java.io.PrintWriter;
+
+// An object to save the state of the program into a specified file
+public class Saver {
+    private String fileName;
+    private PrintWriter writer;
+
+    public Saver(String fileName) {
+        this.fileName = fileName;
+    }
+
+    // EFFECTS: Save the commands into specified file in JSON format
+    public void write(List<Command> commands) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initialize PrinterWriter and open the file
+    private void open() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: close the file and remove writer
+    private void close() {
+
+    }
+}
