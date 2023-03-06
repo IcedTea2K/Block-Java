875f30770a5915e775a75093360e239782ef4fde
Minh Au <auduongminh2004@gmail.com>
2023-03-05 21:28:41 -0800

    add tests for constructor

--- src/test/model/SaverTest.java
+++ src/test/model/SaverTest.java
@@ -1,4 +1,25 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import persistence.Saver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SaverTest {
+    private Saver testSaver;
+    private String targetFile;
+
+    @BeforeEach
+    public void setup() {
+        targetFile = "saved_progress.json";
+        testSaver = new Saver(targetFile);
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals(targetFile, testSaver.getFileName());
+        assertEquals("JOJI.json", testSaver.getFileName());
+    }
+
 }
