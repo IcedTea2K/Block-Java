ff2f3d6b6d47933a60c407400fc34ceabdf4df32
Minh Au <auduongminh2004@gmail.com>
2023-03-06 09:27:30 -0800

    add tests for reading

--- src/test/model/LoaderTest.java
+++ src/test/model/LoaderTest.java
@@ -1,10 +1,14 @@
 package model;
 
 import except.InvalidArgumentException;
+import except.NotYetExecutedException;
+import except.WarningException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import persistence.Loader;
+import persistence.Saver;
 
+import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
@@ -12,39 +16,65 @@ import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LoaderTest {
-    private String targetFile;
+    private String nonEmptyDataFile;
+    private String emptyDataFile;
     private Loader testLoader;
+    private Saver helpingSaver;
     private List<Command> exampleData;
 
     @BeforeEach
     public void setup() {
-        targetFile = "example_data.json";
-        testLoader = new Loader(targetFile);
-        exampleData = loadExampleData();
+        nonEmptyDataFile = "./data/example_data_one.json";
+        emptyDataFile = "./data/example_data_two.json";
+        exampleData = createExampleData();
+        writeExampleData(new LinkedList<Command>(), emptyDataFile);
     }
 
     @Test
     public void testConstructor() {
-        assertEquals(targetFile, testLoader.getFileName());
+        testLoader = new Loader(nonEmptyDataFile);
+        assertEquals(nonEmptyDataFile, testLoader.getFileName());
         assertFalse("JOJI.json" == testLoader.getFileName());
     }
 
     @Test
     public void testReadInvalidFile() {
-
+        testLoader = new Loader("Invalid Path.json");
+        List<Command> loadedCommands;
+        try {
+            loadedCommands = testLoader.read();
+            fail("FileNotFoundException should be raised");
+        } catch (FileNotFoundException e) {
+            // pass the test
+        }
     }
 
     @Test
     public void testReadEmptyFile() {
-
+        testLoader = new Loader(emptyDataFile);
+        List<Command> loadedCommands = null;
+        try {
+            testLoader.read();
+        } catch (FileNotFoundException e) {
+            fail("No exception should be raised");
+        }
+        assertEquals(0, loadedCommands.size());
     }
 
     @Test
     public void testReadFile() {
-        
+        testLoader = new Loader(nonEmptyDataFile);
+        List<Command> loadedCommands = null;
+        try {
+            testLoader.read();
+        } catch (FileNotFoundException e) {
+            fail("No exception should be raised");
+        }
+        assertTrue(compareCommands(exampleData, loadedCommands));
     }
 
-    private List<Command> loadExampleData() {
+    // EFFECTS: load example data manually
+    private List<Command> createExampleData() {
         Random numGenerator = new Random(9801);
         List<Command> addedCommands = new LinkedList<>();
         for (int i = 0; i < 20; i++) {
@@ -75,6 +105,31 @@ public class LoaderTest {
 
             addedCommands.add(command);
         }
+        writeExampleData(addedCommands, nonEmptyDataFile);
         return addedCommands;
+    }
+
+    private void writeExampleData(List<Command> commands, String targetFile) {
+        helpingSaver = new Saver(targetFile);
+        try {
+            helpingSaver.write(commands, true);
+        } catch (FileNotFoundException e) {
+            fail("No exception should be raised");
+        } catch (NotYetExecutedException e) {
+            fail("No exception should be raised");
+        } catch (WarningException e) {
+            fail("No exception should be raised");
+        }
+        helpingSaver = null;
+    }
+
+    private boolean compareCommands(List<Command> listOne, List<Command> listTwo) {
+        if (listOne.size() != listTwo.size())
+            return false;
+        for (int i = 0; i < listOne.size(); i++) {
+            if(!listOne.get(i).equals(listTwo.get(i)))
+                return false;
+        }
+        return true;
     }
 }
