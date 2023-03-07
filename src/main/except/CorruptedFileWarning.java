1a3895b3997ddab2db559da8c3a039d171317e23
Minh Au <auduongminh2004@gmail.com>
2023-03-07 09:14:43 -0800

    edit warning message

--- src/main/except/CorruptedFileWarning.java
+++ src/main/except/CorruptedFileWarning.java
@@ -2,7 +2,14 @@ package except;
 
 // Warns the user about the potential corrupted file
 public class CorruptedFileWarning extends WarningException {
+    // EFFECTS: construct a warning when there's an internal reading exception (IOException)
     public CorruptedFileWarning(String fileName) {
-        super("Warning: The file " + fileName + " might be corrupted or not found.");
+        super("Warning: The file " + fileName + " can not be resolved.");
+    }
+
+    // EFFECTS: construct a message when there's an error parsing the file
+    public CorruptedFileWarning(String fileName, int objectPosition) {
+        super("Warning: Object at position " + objectPosition + " can not be resolved. The file "
+                + fileName + " might be corrupted.");
     }
 }
