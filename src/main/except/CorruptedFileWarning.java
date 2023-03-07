d0b24d290e2d06c0b0e43cdf64b49c01c4d85558
Minh Au <auduongminh2004@gmail.com>
2023-03-07 09:05:48 -0800

    edit warning message

--- src/main/except/CorruptedFileWarning.java
+++ src/main/except/CorruptedFileWarning.java
@@ -3,6 +3,6 @@ package except;
 // Warns the user about the potential corrupted file
 public class CorruptedFileWarning extends WarningException {
     public CorruptedFileWarning(String fileName) {
-        super("Warning: The file " + fileName + " might be corrupted.");
+        super("Warning: The file " + fileName + " might be corrupted or not found.");
     }
 }
