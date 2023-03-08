564d4b384b22ef2f4dcf321d8cca86dd28f70170
Minh Au <auduongminh2004@gmail.com>
2023-03-07 16:39:19 -0800

    make warning a normal exception (not abstract) to allow UI to create general warning whenever possible

--- src/main/except/WarningException.java
+++ src/main/except/WarningException.java
@@ -1,7 +1,7 @@
 package except;
 
 // Exception for warning raised by the program
-public abstract class WarningException extends Exception {
+public class WarningException extends Exception {
     public WarningException(String msg) {
         super(msg);
     }
