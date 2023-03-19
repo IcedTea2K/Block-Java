e6d015e9ea023b5b803d9b0b48c0090f7d9c7ad1
Minh Au <auduongminh2004@gmail.com>
2023-03-18 23:18:34 -0700

    implement logical class

--- src/main/model/Logical.java
+++ src/main/model/Logical.java
@@ -2,4 +2,17 @@ package model;
 
 // Type of commands that take in two booleans and produce a boolean
 public abstract class Logical extends Bool {
+    @Override
+    // EFFECTS: return the constraints of this command
+    public String getConstraints() {
+        return super.getConstraints() + " They both need to be numbers.";
+    }
+
+    @Override
+    // EFFECTS: turn the command into a string
+    public String toString() {
+        String msg = getHeader().split(" ")[0] + " "
+                + operandOne.getNumber() + " " + operandTwo.getNumber();
+        return msg;
+    }
 }
