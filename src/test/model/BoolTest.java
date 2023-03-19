72575f98ccac172c42b808a68a44c41baf0e09f7
Minh Au <auduongminh2004@gmail.com>
2023-03-18 19:28:09 -0700

    remove some more bool tests

--- src/test/model/BoolTest.java
+++ src/test/model/BoolTest.java
@@ -5,42 +5,11 @@ import except.InvalidReturnTypeException;
 import except.MissingArgumentException;
 import except.NotYetExecutedException;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public abstract class BoolTest extends OperatorTest {
     protected Bool command;
-
-    @Override
-    public void testInputWrongType() {
-        DataType tempNum = new DataType(12);
-        DataType tempStream = new DataType(new ArrayList<DataType>());
-
-        try {
-            command.input(tempNum, tempStream);
-            fail("InvalidArgumentException should have been raised");
-        } catch (InvalidArgumentException e) {
-            assertEquals("except.WrongArgumentTypeException: " +
-                    "Expecting boolean Received number", e.toString());
-        }
-
-        try {
-            command.input(tempStream, tempNum);
-            fail("InvalidArgumentException should have been raised");
-        } catch (InvalidArgumentException e) {
-            assertEquals("except.WrongArgumentTypeException: " +
-                    "Expecting boolean Received data stream", e.toString());
-        }
-    }
-
-    @Override
-    public void testGetConstraints() {
-        assertEquals("Operators only accept two inputs. They both need to be boolean.",
-                command.getConstraints());
-    }
 
     @Override
     public void testGetReturnType() {
