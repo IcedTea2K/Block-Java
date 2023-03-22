e01efb07c1cbc26ba484e46307d81476b118685d
Minh Au <auduongminh2004@gmail.com>
2023-03-21 23:34:42 -0700

    divide the view into different sections and add the layout for the different views/panes

--- src/main/ui/GraphicalApplication.java
+++ src/main/ui/GraphicalApplication.java
@@ -7,6 +7,7 @@ import java.awt.*;
 public class GraphicalApplication extends JFrame {
     public static final int WIDTH = 980;
     public static final int HEIGHT = 750;
+    private JPanel mainPane;
 
     // EFFECTS: initialize the GUI
     public GraphicalApplication() {
@@ -15,8 +16,115 @@ public class GraphicalApplication extends JFrame {
         setResizable(false);
         centerOnScreen();
 
+        setPanes();
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: set the different views in the application
+    private void setPanes() {
+        mainPane = new JPanel();
+        mainPane.setLayout(new BoxLayout(mainPane, BoxLayout.X_AXIS));
+        mainPane.setBorder(BorderFactory.createEmptyBorder());
+        setSideBar();
+        setEditor();
+        add(mainPane);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: set the sidebar of the program. It contains the commands and the buttons
+    private void setSideBar() {
+        JPanel sidePane = new JPanel();
+        sidePane.setLayout(new BoxLayout(sidePane, BoxLayout.Y_AXIS));
+        addCommandPane(sidePane);
+        addButtonPane(sidePane);
+        mainPane.add(sidePane, BorderLayout.WEST);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: the main view where the user interact with the program
+    private void setEditor() {
+        JPanel editorPane = new JPanel();
+        editorPane.setLayout(new BoxLayout(editorPane, BoxLayout.Y_AXIS));
+        setUpperPane(editorPane);
+        setLowerPane(editorPane);
+        mainPane.add(editorPane, BorderLayout.EAST);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: set the upper area of the main editor view
+    private void setUpperPane(JPanel editorPane) {
+        JPanel upperPane = new JPanel();
+        upperPane.setLayout(new BoxLayout(upperPane, BoxLayout.X_AXIS));
+        addTranslatorPane(upperPane);
+        addTerminalPane(upperPane);
+        editorPane.add(upperPane, BorderLayout.NORTH);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: set the lower pane of the editor view
+    private void setLowerPane(JPanel editorPane) {
+        JPanel lowerPane = new JPanel();
+        lowerPane.setLayout(new BoxLayout(lowerPane, BoxLayout.X_AXIS));
+        addJavaPane(lowerPane);
+        editorPane.add(lowerPane, BorderLayout.NORTH);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: add the translator pane
+    private void addTranslatorPane(JPanel container) {
+        JPanel translatorPane = new JPanel();
+        translatorPane.setPreferredSize(new Dimension(300, 300));
+        translatorPane.setBackground(Color.gray);
+
+        translatorPane.add(new JLabel("Translator"));
+        container.add(translatorPane, BorderLayout.WEST);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: add the terminal pane
+    private void addTerminalPane(JPanel container) {
+        JPanel terminalPane = new JPanel();
+        terminalPane.setPreferredSize(new Dimension(300, 300));
+        terminalPane.setBackground(Color.yellow);
+
+        terminalPane.add(new JLabel("Terminal"));
+        container.add(terminalPane, BorderLayout.EAST);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: add the java pane
+    private void addJavaPane(JPanel container) {
+        JPanel javaPane = new JPanel();
+        javaPane.setPreferredSize(new Dimension(300, 300));
+        javaPane.setBackground(Color.BLUE);
+
+        javaPane.add(new JLabel("Java View"));
+        container.add(javaPane, BorderLayout.EAST);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: add area that contains all the commands
+    private void addCommandPane(JPanel container) {
+        JPanel commandPane = new JPanel();
+        commandPane.setPreferredSize(new Dimension(300, 300));
+        commandPane.setBackground(Color.RED);
+
+        commandPane.add(new JLabel("Commands"));
+        container.add(commandPane, BorderLayout.NORTH);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: add area that contains all the buttons
+    private void addButtonPane(JPanel container) {
+        JPanel buttonPane = new JPanel();
+        buttonPane.setPreferredSize(new Dimension(300, 300));
+        buttonPane.setBackground(Color.CYAN);
+
+        buttonPane.add(new JLabel("Buttons"));
+        container.add(buttonPane, BorderLayout.NORTH);
     }
 
     // MODIFIES: this
@@ -24,6 +132,6 @@ public class GraphicalApplication extends JFrame {
     private void centerOnScreen() {
         int width = Toolkit.getDefaultToolkit().getScreenSize().width;
         int height = Toolkit.getDefaultToolkit().getScreenSize().height;
-        setLocation((width - getWidth())/2, (height - getHeight())/2);
+        setLocation((width - getWidth()) / 2, (height - getHeight()) / 2);
     }
 }
